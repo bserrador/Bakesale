5ada8f27739c15419a692c9dae2baccb689f64c0
Brittany Serrador <brittanyserrador@gmail.com>
2018-02-13 10:46:31 -0500

    Added API call

--- Bakesale/src/components/App.js
+++ Bakesale/src/components/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ajax from '../ajax';
+import DealList from './DealList';
 
 class App extends React.Component {
+    state = {
+        deals: [],
+    };
+
+    async componentDidMount() {
+        const deals = await ajax.fetchInitialDeals();
+        this.setState((prevState) => {
+            return { deals };
+        });
+    }
+
     render() {
         return (
-            <View></View>
+            <View style={styles.container}>
+                {this.state.deals.length > 0 ? (
+                    <DealList deals={this.state.deals} />
+                ) : (
+                    <Text style={styles.header}>Bakesale</Text>
+                )}
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        fontSize: 40,
+    },
+});
 
 export default App;
---
+++ Bakesale/src/components/DealList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class DealList extends Component {
+  render() {
+    return (
+        <View>
+            <Text>Deals...</Text>
+        </View>
+    );
+  }
+}
+
+export default DealList;
