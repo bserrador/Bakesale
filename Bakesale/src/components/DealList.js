c5f5a3cb0d82facab6b2bec9c484bcc0ded70af3
Brittany Serrador <brittanyserrador@gmail.com>
2018-02-13 11:11:59 -0500

    Display data in FlatList

--- Bakesale/src/components/DealList.js
+++ Bakesale/src/components/DealList.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+
 
 class DealList extends Component {
-  render() {
-    return (
-        <View>
-            <Text>Deals...</Text>
-        </View>
-    );
-  }
+    static propTypes = {
+        deals: PropTypes.array.isRequired,
+    };
+
+    render() {
+        return (
+            <View style={styles.list}>
+                <FlatList
+                    data={this.props.deals}
+                    renderItem={({item}) => <Text>{item.title}</Text>}
+                />
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    list: {
+        backgroundColor: '#eee',
+        flex: 1,
+        width: '100%',
+        paddingTop: 50,
+    },
+});
 
 export default DealList;
